3f62c4cae252fc7efbe046519340e175b54448fd
Taxi_tabby <rkdmf0000@gmail.com>
2025-08-04 11:38:59 +0900

    feat: ConstantDB 클래스에 기본값 상수 추가

--- src/app/injectable/constant/db.module.ts
+++ src/app/injectable/constant/db.module.ts
@@ -2,12 +2,40 @@
 
 export default class ConstantDB {
     
-
     /**
      * UUID의 비어있는 기본값 예시
      */
     public EMPTY_UUID = '00000000-0000-0000-0000-000000000000';
 
+    /**
+     * Ingeter의 최대 값
+     */
+    public MAX_INTEGER = 2147483647;
+
+    /**
+     * Ingeter의 최소 값
+     */
+    public MIN_INTEGER = -2147483648;
+
+    /**
+     * Boolean의 기본값
+     */
+    public DEFAULT_BOOLEAN = false;
+
+    /**
+     * String의 기본값
+     */
+    public DEFAULT_STRING = '';
+
+    /**
+     * BigInt의 최대 값
+     */
+    public MAX_BIGINT = BigInt('9223372036854775807');
+
+    /**
+     * BigInt의 최소 값
+     */
+    public MIN_BIGINT = BigInt('-9223372036854775808');
 }
 
 
